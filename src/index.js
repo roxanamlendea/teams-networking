8a74557fdd86b189eb553345a647cd3de26a91ea
Roxana Mlendea <roxanamlendea@gmail.com>
2023-08-01 17:30:15 +0300

    deploy scripts

--- src/index.js
+++ src/index.js
@@ -110,7 +110,7 @@ function addTitlesToOverflowCells() {
 function loadTeams() {
   let url = "http://localhost:3000/teams-json";
   if (window.location.host === "roxanamlendea.github.io") {
-    url = "teams.json";
+    url = "data/teams.json";
     console.info("display mock data %o", url);
   }
   fetch(url)
