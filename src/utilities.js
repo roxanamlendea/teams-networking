03766e6a4e40c222ab2462013e1efa086d121267
Roxana Mlendea <roxanamlendea@gmail.com>
2023-08-04 17:18:23 +0300

    improve mask/unmask using element as selector

--- src/utilities.js
+++ src/utilities.js
@@ -1,18 +1,24 @@
 export function $(selector) {
-    return document.querySelector(selector);
+  return document.querySelector(selector);
+}
+
+export function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+export function mask(el) {
+  if (typeof el === "string") {
+    el = $(el);
   }
-  
-  export function sleep(ms) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve();
-      }, ms);
-    });
+  el.classList.add("loading-mask");
+}
+export function unmask(el) {
+  if (typeof el === "string") {
+    el = $(el);
   }
-  
-  export function mask(el) {
-    el.classList.add("loading-mask");
-  }
-  export function unmask(el) {
-    el.classList.remove("loading-mask");
-  }
+  el.classList.remove("loading-mask");
+}
