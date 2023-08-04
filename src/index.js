8255fd133795a2a32b1f0abf4a1e71a0dce68e2a
Roxana Mlendea <roxanamlendea@gmail.com>
2023-08-04 18:31:59 +0300

    use common code after 2 function that use await

--- src/index.js
+++ src/index.js
@@ -112,8 +112,6 @@ async function onSubmit(e) {
 
   if (editId) {
     team.id = editId;
-    console.warn("update...", team);
-
     const { success } = await updateTeamRequest(team);
     if (success) {
       allTeams = allTeams.map(t => {
@@ -126,22 +124,18 @@ async function onSubmit(e) {
         }
         return t;
       });
-      console.info(allTeams);
-      renderTeams(allTeams);
       setInputsDisabled(false);
       editId = "";
     }
-    unmask(form);
   } else {
     const { success, id } = await createTeamRequest(team);
     if (success) {
       team.id = id;
       allTeams = [...allTeams, team];
-      renderTeams(allTeams);
       $(form).reset();
     }
-    unmask(form);
   }
+  renderTeams(allTeams);
   unmask(form);
 }
 
@@ -155,7 +149,7 @@ function startEdit(id) {
 }
 
 function setInputsDisabled(disabled) {
-  document.querySelectorAll("tfoot input").forEach(input => {
+  document.querySelectorAll("tfoot input, tfoot button").forEach(input => {
     input.disabled = disabled;
   });
 }
@@ -193,18 +187,17 @@ function initEvents() {
     }
   });
 
-  $("#teamsTable tbody").addEventListener("click", e => {
+  $("#teamsTable tbody").addEventListener("click", async e => {
     if (e.target.matches("button.delete-btn")) {
       const id = e.target.dataset.id;
       //console.warn("delete... %o", id);
       mask(form);
-      deleteTeamRequest(id, status => {
-        console.info("delete callback %o", status);
-        if (status.success) {
-          //window.location.reload();
-          loadTeams();
-        }
-      });
+      const status = await deleteTeamRequest(id);
+      console.info("delete callback %o", status);
+      if (status.success) {
+        //window.location.reload();
+        loadTeams();
+      }
     } else if (e.target.matches("button.edit-btn")) {
       const id = e.target.dataset.id;
       startEdit(id);
